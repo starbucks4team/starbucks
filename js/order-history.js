e88101f9cb7bbe6d14719df93659d219da3ae2cc
cheonkio <heelun8525@naver.com>
2025-03-13 17:26:22 +0900

    product list, order history front code

---
+++ js/order-history.js
@@ -0,0 +1,43 @@
+async function getOrderHistory(url) {
+  try {
+    const response = await fetch(url, { method: 'GET' });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return [];
+  }
+}
+
+window.onload = async () => {
+  let result = await getOrderHistory('http://127.0.0.1:3000/orderhistory');
+  for (let i = 0; i < result.length; i++) {
+    let container = document.querySelector('#order-table>tbody');
+    let template = `<tr class="underline">
+            <td class="order-cell">${result[i].orderNo}</td>
+            <td class="order-cell">${result[i].orderDate}</td>
+            <td class="order-cell">`;
+
+    for (let j = 0; j < result[i].prodNos.length; j++) {
+      template += `${result[i].prodNos[j]}<br/>`;
+    }
+    template += `</td> <td class="order-cell">`;
+    for (let j = 0; j < result[i].prodNames.length; j++) {
+      template += `${result[i].prodNames[j]}<br/>`;
+    }
+    template += `</td> <td class="order-cell">`;
+    for (let j = 0; j < result[i].prices.length; j++) {
+      template += `${result[i].prices[j]}<br/>`;
+    }
+    template += `</td> <td class="order-cell">`;
+    for (let j = 0; j < result[i].quantities.length; j++) {
+      template += `${result[i].quantities[j]}<br/>`;
+    }
+    template += `</td> <td class="order-cell">`;
+    template += `</td></tr>`;
+    container.innerHTML += template;
+  }
+};
