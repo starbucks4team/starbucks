0b90e25f9b60eecb42d1c82e6ea9eee7b3829486
cheonkio <heelun8525@naver.com>
2025-03-18 16:39:59 +0900

    Feat: Add header load js

---
+++ front/js/header.js
@@ -0,0 +1,13 @@
+async function fetchHtmlAsText(url) {
+  return await (await fetch(url)).text();
+}
+
+async function importPage(target) {
+  document.querySelector('#' + target).innerHTML = await fetchHtmlAsText(
+    '../components/' + target + '.html'
+  );
+}
+
+window.onload = () => {
+  importPage('header');
+};
